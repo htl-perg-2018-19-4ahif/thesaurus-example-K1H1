c988328a2a210ff058644bfe3ca935eb48e45e27
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-11-20 20:19:13 +0100

    -Reading in the .txt- file

--- thesaurus.ts
+++ thesaurus.ts
@@ -1,34 +1,50 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inputArguments = process.argv.slice(2);
-var file = 'openthesaurus.txt';
-var found = false;
-var fs = require("fs"); //needed for reading in the file
-var input = [];
+let inputArguments: string[] = process.argv.slice(2);
+let file: string = 'openthesaurus.txt'
+let found: boolean = false;
+
+import fs = require('fs');              //needed for reading in the file
+
+
+
+var input: string[] = [];
+
 //Write error message when there is no word-input
 if (inputArguments.length === 0) {
     process.stdout.write("Please specify words!");
-}
-else {
+} else {
     //split out the words of the console input:
-    for (var i = 0; i < inputArguments.length; i++) {
+    for (let i = 0; i < inputArguments.length; i++) {
         input[i] = inputArguments[i];
         console.log(input[i]);
+
     }
+
     readFile();
     findSynonym(input);
 }
+
+
+
+
 //read in document bzw. line:
 function readFile() {
     fs.readFile(file, 'utf8', function (err, data) {
-        if (err)
-            console.log(err);
+        if (err) console.log(err);
         console.log('OK: ' + file);
-        console.log(data);
+        console.log(data)
     });
+
+
+
 }
+
 //look if synonym is in the list:
-function findSynonym(input) {
-    for (var i = 0; i < input.length; i++) {
+function findSynonym(input: string[]) {
+    for (let i = 0; i < input.length; i++) {
+
     }
+
+
+
 }
+
